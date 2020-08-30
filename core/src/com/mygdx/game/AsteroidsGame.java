416b85d8b93e43416b9257e8cd88924f4b52047f
martin.stancl <mstancl94@gmail.com>
2020-08-30 18:57:04 +0100

    - working version

--- core/src/com/mygdx/game/AsteroidsGame.java
+++ core/src/com/mygdx/game/AsteroidsGame.java
@@ -13,6 +13,11 @@ public class AsteroidsGame extends Game {
 
     public Stage mainStage;
     private BaseActor spaceShip;
+    private BaseActor projectile;
+
+    float secondsSinceUpPressed;
+    long startSecondsWhenUpPressed;
+
 
     @Override
     public void create() {
@@ -26,27 +31,71 @@ public class AsteroidsGame extends Game {
         spaceShip = new BaseActor();
         spaceShip.setTexture(new Texture(Gdx.files.internal("spaceShip.png")));
         spaceShip.setPosition(mainStage.getWidth() / 2, mainStage.getHeight() / 2);
-        spaceShip.setOrigin(spaceShip.getWidth()/2,spaceShip.getHeight()/2);
+        spaceShip.setOrigin(spaceShip.getWidth() / 2, spaceShip.getHeight() / 2);
         mainStage.addActor(spaceShip);
 
+
+        projectile = new BaseActor();
+        projectile.setTexture(new Texture(Gdx.files.internal("projectile.png")));
+        projectile.setPosition(mainStage.getWidth() / 2, mainStage.getHeight() / 2);
+        projectile.setVisible(false);
+        mainStage.addActor(projectile);
+
+
+        spaceShip.velocityX = 0;
+        spaceShip.velocityY = 0;
 
     }
 
     @Override
     public void render() {
+
         if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            spaceShip.velocityY += 2;
-        
-        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            spaceShip.velocityY -= 2;
-        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-          spaceShip.rotateBy(2);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            secondsSinceUpPressed = (System.currentTimeMillis() - startSecondsWhenUpPressed) / 1000F;
+            float rotation = spaceShip.getRotation();
+            spaceShip.velocityX += -(float) (Math.sin(Math.toRadians(rotation)) * Gdx.graphics.getDeltaTime() * secondsSinceUpPressed);
+            spaceShip.velocityY += (float) (Math.cos(Math.toRadians(rotation)) * Gdx.graphics.getDeltaTime() * secondsSinceUpPressed);
+        } else {
+            startSecondsWhenUpPressed = System.currentTimeMillis();
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            spaceShip.rotateBy(2);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             spaceShip.rotateBy(-2);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+
+            float projectileX = (spaceShip.getX() + spaceShip.getWidth() / 2);
+            float projectileY = (spaceShip.getY() + spaceShip.getHeight() / 2);
+
+            projectile.setRotation(spaceShip.getRotation());
+            projectile.setPosition(projectileX, projectileY);
+            projectile.setVisible(true);
+
+            projectile.velocityX = -(float) (Math.sin(Math.toRadians(projectile.getRotation())) * Gdx.graphics.getDeltaTime()) * 300;
+            projectile.velocityY = (float) (Math.cos(Math.toRadians(projectile.getRotation())) * Gdx.graphics.getDeltaTime()) * 300;
+
         }
 
 
-        spaceShip.moveBy(spaceShip.velocityX * Gdx.graphics.getDeltaTime(), spaceShip.velocityY * Gdx.graphics.getDeltaTime());
+        if (spaceShip.getY() + spaceShip.getHeight() < 0) {
+            spaceShip.setPosition(spaceShip.getX(), mainStage.getHeight());
+        } else if (spaceShip.getY() > mainStage.getHeight()) {
+            spaceShip.setPosition(spaceShip.getX(), -spaceShip.getHeight());
+        } else if (spaceShip.getX() > mainStage.getWidth()) {
+            spaceShip.setPosition(-spaceShip.getWidth(), spaceShip.getY());
+        } else if (spaceShip.getX() + spaceShip.getWidth() < 0) {
+            spaceShip.setPosition(mainStage.getWidth(), spaceShip.getY());
+        }
+
+        spaceShip.moveBy(spaceShip.velocityX, spaceShip.velocityY);
+
+        if (projectile.isVisible()) {
+            projectile.moveBy(projectile.velocityX, projectile.velocityY);
+        }
+
 
         mainStage.act();
         Gdx.gl.glClearColor(0.8f, 0.8f, 1, 1);
